e0efcea1e85a571eae10f29c4451d6614ae82def
lan <Jaksonkoda1284@gmail.com>
2023-06-12 21:06:19 +0800

    fix: init

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // 单行代码的最大宽度
+  printWidth: 500,
+  // 每行结束都加上分号
+  semi: true,
+  // 缩进Vue文件中
+  vueIndentScriptAndStyle: true,
+  // 单引号 true，适用双引号 false
+  singleQuote: true,
+  // 使用默认的折行标准-不折行
+  proseWrap: "never",
+  // 箭头函数，只有一个参数的时候，也需要括号
+  arrowParens: "always",
+  //设置统一的行结尾样式
+  endOfLine: "auto",
+  // 在任何可能的多行中输入尾逗号
+  trailingComma: "none",
+  // 对象的 key 仅在必要时用引号
+  quoteProps: "as-needed",
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: "css",
+  //>不另起一行
+  bracketSameLine: true,
+};
